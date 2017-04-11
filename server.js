403fc953056691e52e4f8bf01225f5610ff5df2b
istvan747 <istvan747@gmail.com>
2017-04-11 19:09:19 +0200

    create server

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(8081, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Server running at http://%s:%s",host,port);
+});
