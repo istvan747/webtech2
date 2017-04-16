58fb9c98350be653852bee1b36f878b0f53b645a
istvan747 <istvan747@gmail.com>
2017-04-16 17:10:00 +0200

    item update, delete

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -2,7 +2,8 @@ var app = angular.module("myApp", []);
 
 app.controller("myController", function($scope,$http){
 	
-
+	$scope.item = new Object();
+	//$scope.items;
 
 	$scope.listItem = function(){
 		$http.get("/listItem").then(function(resp){
@@ -12,13 +13,130 @@ app.controller("myController", function($scope,$http){
 
 	$scope.listItem();
 
-	$scope.saveItem = function(){
-		$scope.newJSONitem = "{"
-							+ "\"itemNumber\" : \"" + $scope.itemNumber + "\", "
-						 	+ "\"appelation\" : \"" + $scope.appelation + "\", "
-						 	+ "\"quantity\" : \"" + $scope.quantity + "\", "
-						 	+ "\"storage\" : \"" + $scope.storage + "\"}";
-		$http.post("/addItem",$scope.newJSONitem);
-		$scope.listItem();
+	$scope.formValid = function(form, classIndex){
+		
+		var itemNumberField = document.getElementsByClassName("itemnumberFieldDiv");
+		var appelationField = document.getElementsByClassName("appelationFieldDiv");
+		var quantityField = document.getElementsByClassName("quantityFieldDiv");
+		var storageField = document.getElementsByClassName("storageFieldDiv");
+
+		var itemNumberExists = false;
+		var itemNumberEmpty = false;
+		var appelationEmpty = false;
+		var quantityEmpty = false;
+		var storageEmpty = false;
+
+		if(form != "updateItem"){
+			$scope.listItem();
+			for(i in $scope.items){
+				if($scope.items[i].itemNumber == $scope.item.itemNumber){
+					itemNumberExists = true;
+					break;
+				}
+			}
+		}
+
+		if(itemNumberExists){
+			itemNumberField[classIndex].className += " has-error";
+			itemNumberField[classIndex].setAttribute("toggle", "popover");
+			itemNumberField[classIndex].setAttribute("title", "Item number is exists!");			
+		}else if($scope.item.itemNumber == undefined || $scope.item.itemNumber == ""){
+			itemNumberEmpty = true;
+			itemNumberField[classIndex].className += " has-error";
+			itemNumberField[classIndex].setAttribute("toggle", "popover");
+			itemNumberField[classIndex].setAttribute("title", "Item number is exists!");
+		}else{
+			itemNumberEmpty = false;
+			itemNumberExists = false;
+			itemNumberField[classIndex].removeAttribute("toggle");
+			itemNumberField[classIndex].removeAttribute("title");
+			itemNumberField[classIndex].classList.remove("has-error");
+		}
+
+		if($scope.item.appelation == undefined || $scope.item.appelation ==""){
+			appelationEmpty = true;
+			appelationField[classIndex].className += " has-error";
+			appelationField[classIndex].setAttribute("toggle", "popover");
+			appelationField[classIndex].setAttribute("title", "Appelation field is empty!");
+		}else{
+			appelationEmpty = false;
+			appelationField[classIndex].removeAttribute("toggle");
+			appelationField[classIndex].removeAttribute("title");
+			appelationField[classIndex].classList.remove("has-error");
+		}
+
+		if($scope.item.quantity == undefined || $scope.item.quantity == ""){
+			quantityEmpty = true;
+			quantityField[classIndex].className += " has-error";
+			quantityField[classIndex].setAttribute("toggle", "popover");
+			quantityField[classIndex].setAttribute("title", "Quantity field is empty!");
+		}else{
+			quantityEmpty = false;
+			quantityField[classIndex].removeAttribute("toggle");
+			quantityField[classIndex].removeAttribute("title");
+			quantityField[classIndex].classList.remove("has-error");
+		}
+
+		if($scope.item.storage == undefined || $scope.item.storage == ""){
+			storageEmpty = true;
+			storageField[classIndex].className += " has-error";
+			storageField[classIndex].setAttribute("toggle", "popover");
+			storageField[classIndex].setAttribute("title", "Storage field is empty!");
+		}else{
+			storageEmpty = false;
+			storageField[classIndex].removeAttribute("toggle");
+			storageField[classIndex].removeAttribute("title");
+			storageField[classIndex].classList.remove("has-error");
+		}
+
+		return !itemNumberExists && !itemNumberEmpty && !appelationEmpty && !quantityEmpty && !storageEmpty;
+	}
+
+	$scope.addItem = function(){
+		if($scope.formValid("addItem",0)){
+			$http.post("/addItem",JSON.stringify($scope.item)).then(function mySucces(response){
+				$scope.listItem();
+				$scope.unSelectItem();
+			}, function myError(response){
+
+			});
+		}
 	};
+
+	$scope.selectItem = function(selectedItem){
+		$scope.item = selectedItem;
+
+	};
+
+	$scope.unSelectItem = function(){
+		$scope.item.itemNumber = "";
+		$scope.item.appelation = "";
+		$scope.item.quantity = "";
+		$scope.item.storage = "";
+	}
+
+	$scope.deleteItem = function(){
+		$http.get("/deleteitem?itemnumber="+$scope.item.itemNumber).then(function mySucces(response){
+			$scope.listItem();
+			$scope.unSelectItem();
+		}, function myError(response){
+
+		});
+	};
+
+	$scope.updateItem = function(){
+		if($scope.formValid("updateItem",1)){
+			$http.post("/updateItem",$scope.item).then(function mySucces(response){
+				$scope.listItem();
+				$scope.unSelectItem();
+			}, function myError(response){
+
+			});
+		}
+	};
+
+	$scope.itemToString = function(){
+		console.log("item: " + $scope.item + "\nitem.itemNumber: " + $scope.item.itemNumber + "\nitem.appelation: " + $scope.item.appelation + "\nitem.quantity: " + $scope.item.quantity + "\nitem.storage: " + $scope.item.storage); 
+	}
+
 });
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var fs = require("fs");
 var app = express();
+var JSON_FILE_PATH = __dirname+"/set.json";
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,15 +13,26 @@ app.get("/",function(req,res){
 });
 
 app.post("/addItem",function(req,res){
-	var items = JSON.parse(fs.readFileSync(__dirname+"/set.json"));
-	items.push(req.body);
-	fs.writeFile(__dirname+"/set.json",JSON.stringify(items),function(err){
-		if(err){
-			return console.error(err);
+	var items = itemsReadFromJSONFile(JSON_FILE_PATH);
+	var reqItem = req.body;
+	var exists = false;
+	for(i in items){
+		var item = items[i];
+		if(item.itemNumber == reqItem.itemNumber){
+			exists = true;
+			break;
 		}
-		console.log("Data writen is successfully!");
-	});
-	res.send("");
+	}
+	if(!exists){
+		items.push(req.body);
+		itemsWriteToJSONFile(items);
+		console.log("Item update is successfully!");
+		res.send("");
+	}else{
+		console.log("Item is exists!");
+		res.send("");		
+	}
+
 });
 
 app.get("/deleteitem",function(req,res){
@@ -31,26 +43,47 @@ app.get("/deleteitem",function(req,res){
 		return;
 	}
 	console.log("delete is run ...");
-	var items = JSON.parse(fs.readFileSync(__dirname+"/set.json"));
+	var items = itemsReadFromJSONFile(JSON_FILE_PATH);
 	items = items.filter(function(item){ return item.itemNumber != req.query['itemnumber'] });
-	console.log("Data delete is successfully!");
-	fs.writeFile(__dirname+"/set.json",JSON.stringify(items),function(err){
-		if(err){
-			return console.error(err);
-		}
-		console.log("Data writen is successfully!");
-	});
+	itemsWriteToJSONFile(items);
+	console.log("Item delete is successfully!");
 	res.send("");
 });
 
-app.get("/updateItem",function(req,res){
-
+app.post("/updateItem",function(req,res){
+	var reqItem = req.body;
+	var items = itemsReadFromJSONFile(JSON_FILE_PATH);
+	for(i in items){
+		var item = items[i];
+		if(item.itemNumber == reqItem.itemNumber){
+			item.appelation = reqItem.appelation;
+			item.quantity = reqItem.quantity;
+			item.storage = reqItem.storage;
+			itemsWriteToJSONFile(items);
+			break;		
+		}
+	}
+	console.log("Item update is successfully!");
+	res.send("");
 });
 
 app.get("/listItem",function(req,res){
 	var items = JSON.parse(fs.readFileSync(__dirname+"/set.json"));
 	res.send(items);	
 });
+
+function itemsWriteToJSONFile(items){
+	fs.writeFile(__dirname+"/set.json",JSON.stringify(items),function(err){
+		if(err){
+			return console.error(err);
+		}
+		console.log("Data writen is successfully!");
+	});	
+}
+
+function itemsReadFromJSONFile(file){
+	return JSON.parse(fs.readFileSync(file));
+}
 
 var server = app.listen(8081, function(){
 	var host = server.address().address;
